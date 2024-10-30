fc4fdf04f50669ef0d4301ef4fb8d41594dff6bf
devearley <earleyalex@gmail.com>
2024-10-29 23:31:27 -0700

    work

--- main.ts
+++ main.ts
@@ -1,32 +1,3 @@
 const name = Deno.args[0];
 const food = Deno.args[1];
-console.log(`Hello ${name}, I like ${food}!`);
-
-// import { parseArgs } from "jsr:@std/cli/parse-args";
-
-// const flags = parseArgs(Deno.args, {
-//   boolean: ["help", "color"],
-//   string: ["version"],
-//   default: { color: true },
-//   negatable: ["color"],
-// });
-
- await Deno.writeTextFile("hello.txt", name + "|" + food);
-
-// const file = await Deno.create("hello.txt");
-
-// const writer = file.writable.getWriter();
-// await writer.write(new TextEncoder().encode("World!"));
-
-// await writer.close();
-
-console.log("done");
-setTimeout(() => {
-  console.log(".");
-}, 1000);
-setTimeout(() => {
-  console.log("..");
-}, 1500);
-setTimeout(() => {
-  console.log("...");
-}, 2000);
+await Deno.writeTextFile("C:\\Users\\TheDean\\Desktop\\build-logs.txt", name + "|" + food);
